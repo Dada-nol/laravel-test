505629d6f62c8bafa3fa4adfcc83255916874543
Darren <nardol.darren@gmail.com>
2025-03-01 20:00:00 +0100

    CREATE nav for guest user

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+
+<x-app-layout>
+  <h2 class="text-lg font-medium text-gray-900">{{__("Je suis dans la page d'accueil !") }}</h2>
+  <p>{{__('Polala ça marche perfecto') }}</p>
+</x-app-layout>
